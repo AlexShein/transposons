97060934e27aee08c232d088519c0ccbcfd87e85
Alexander Shein <mr.ashein@gmail.com>
2018-09-04 22:47:51 +0300

    Bash script fixes

--- shell_scripts/cut_last_50_bp.sh
+++ shell_scripts/cut_last_50_bp.sh
@@ -4,11 +4,13 @@
 # Third arg: int
 # Example: shell_scripts/cut_last_50_bp.sh $(pwd) ./test.txt 50
 echo "Writing to "$2
-LEN=$(ls $1*.fa|wc -l)
+LEN=$(ls $1/*.fa|wc -l)
 COUNTER=1
 for filename in $(ls $1/*.fa); do
-    tail -n50 $filename >> "$2"
+    tail -n$3 $filename >> "$2"
     echo '' >> "$2"
-    echo "Processing $COUNTER out of $LEN"
+    if [$COUNTER//10 -eq 0] then
+        echo "Processing $COUNTER out of $LEN"
+    fi
     COUNTER=$[COUNTER + 1]
 done
