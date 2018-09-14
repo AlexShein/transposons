50a1839243703a0d3cbf6e11fb534eb51d6fb7a0
Alexander Shein <mr.ashein@gmail.com>
2018-09-14 20:49:34 +0300

    Another fix

--- shell_scripts/cut_off_poly_a.sh
+++ shell_scripts/cut_off_poly_a.sh
@@ -4,6 +4,6 @@
 for filename in $(ls $1*.fa); do
     POLY_A_END=$(tail -n1 $filename | rev | grep -aob '[CTG]\{2,\}' | head -n1 | grep -oE '[0-9]+')
     # Cut the sequence to the position of last non-A bases pair
-    SEQ=$(tail n1 $filename)
+    SEQ=$(tail -n1 $filename)
     echo $SEQ | cut -c1-$((${#SEQ}-$POLY_A_END)) >> "$2$filename"
 done;
