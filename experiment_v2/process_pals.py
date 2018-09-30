c496eee5c8c5f099652e8e0d24afdf7b6111c3e2
Alexander Shein <mr.ashein@gmail.com>
2018-09-30 22:49:47 +0300

    Modified algorithm

--- experiment_v2/process_pals.py
+++ experiment_v2/process_pals.py
@@ -7,8 +7,8 @@ from functools import partial, reduce
 from multiprocessing import Pool, cpu_count
 from multiprocessing.dummy import Pool as d_Pool
 from operator import add
-from random import randint
 
+import numpy as np
 import pandas as pd
 
 from process_line import process_lines
@@ -26,52 +26,44 @@ def get_chunks(lst, chunk_size):
         yield lst[i:m]
 
 
-def get_last_line(filename):
+def get_last_line(filename, target):
     with open(filename, 'r') as file:
         lines = file.readlines()
-        if lines:
-            return ((lines[-1], 1),)
-    return ()
+        return lines and lines[-1] and ((lines[-1], target),) or ()
 
 
-def get_random_line(filename):
+def get_first_line(filename, target):
     with open(filename, 'r') as file:
-        lines = file.readlines()
-        if lines:
-            return ((lines[randint(0, len(lines) - 1)], 0),)
-    return ()
+        line = file.readline()
+        return line and ((line, target),) or ()
 
 
-def get_lines(filename, n=0):
-    result = ()
-    with open(filename, 'r') as file:
-        if n:
-            # Little magic to get rid of empty lines
-            result = tuple(filter(lambda x: bool(x[0]), ((file.readline(), 0) for _ in range(n))))
-        else:
-            result = tuple((line, 0) for line in file if line)
-    return result
-
-
-def get_lines_test(filename):
+def get_random_lines(filename, target=0, n_lines=0, omit_first=False, omit_last=False):
     """
     returns tuple of pairs like (line, 0/1), where 0/1 shows whether this sequence is target one or not
     Target one is in last line of file
     Non-target one is a random line of file
     """
     with open(filename, 'r') as file:
-        lines = file.readlines()
+        lines = [line for line in filter(bool, file.readlines())]
         if lines:
-            return ((lines[-1], 1), (lines[randint(0, len(lines) - 1)], 0))
-        else:
-            return ()
+            if omit_first:
+                lines = lines[1:]
+            if omit_last:
+                lines = lines[:-1]
+            if n_lines and n_lines <= len(lines):
+                return [(line, target) for line in np.random.choice(lines, size=n_lines, replace=False)]
+            else:
+                return [(line, target) for line in lines]
+    return ()
 
 
 def begin_processing(
     path,
     target,
+    lines,
+    omit,
     output_file='sl_annotation_result.csv',
-    test_run=False,
     n_lines=0,
 ):
     start = dt.utcnow()
@@ -82,19 +74,31 @@ def begin_processing(
         ) for filename in os.listdir(path) if filename[-4:] == '.pal'
     ]
     log.info("Got {0} files".format(len(files)))
-    if test_run:
-        line_retiever_func = get_lines_test
-    elif target:
-        # processing target files only
-        line_retiever_func = get_last_line
+
+    # processing non-target files only
+    if n_lines:
+        file_lines = n_lines // len(files) + 1
+        # TODO allow file skiping to deal with situations when there are more files, than n_lines
     else:
-        # processing non-target files only
-        if n_lines:
-            file_lines = n_lines // len(files) + 1
-            # TODO allow file skiping to deal with situations when there are more files, than n_lines
-        else:
-            file_lines = 0
-        line_retiever_func = partial(get_lines, n=file_lines)
+        file_lines = 0
+
+    omit_last = 'l' in omit
+    omit_first = 'f' in omit
+
+    if lines == 'first':
+        line_retiever_func = partial(get_first_line, target=target)
+    elif lines == 'last':
+        line_retiever_func = partial(get_last_line, target=target)
+    elif lines == 'rand':
+        line_retiever_func = partial(
+            get_random_lines,
+            target=target,
+            n_lines=file_lines,
+            omit_first=omit_first,
+            omit_last=omit_last,
+        )
+    else:
+        return
 
     # Using dummy because threading is good for io-bound operations
     with d_Pool() as d_pool:
@@ -130,7 +134,7 @@ def begin_processing(
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Process *.pal files to store statistics.',
-        usage='python3 process_pals.py -output_file 123456.csv -path ./temp_pal',
+        usage='python3 process_pals.py -output_file 123456.csv -path ./temp_pal -lines rand -o f,l --target',
     )
     parser.add_argument(
         '-output_file',
@@ -153,28 +157,35 @@ if __name__ == '__main__':
         default=False,
     )
     parser.add_argument(
-        '--test',
-        dest='test',
-        action='store_true',
-        help='Test mode',
-        required=False,
-        default=False,
-    )
-    parser.add_argument(
         '-n',
         dest='n_lines',
         help='Number of lines to process',
         required=False,
         default=0,
     )
+    parser.add_argument(
+        '-lines',
+        dest='lines',
+        help='Location of target .pal files',
+        required=True,
+        choices=['first', 'last', 'rand']
+    )
+    parser.add_argument(
+        '-o',
+        dest='omit',
+        help='Which lines to omit, f[irst],l[ast]',
+        required=False,
+        default='',
+    )
     args = parser.parse_args()
-    log.info("Started command, pid {0}, output_file {1}, path {2}, target {3}, test {4}, n_lines {5}".format(
-        os.getpid(), args.output_file, args.path, args.target, args.test, args.n_lines
+    log.info("Started command, pid {0}, output_file {1}, path {2}, target {3}, n_lines {4}, omit {5}".format(
+        os.getpid(), args.output_file, args.path, args.target, args.n_lines, args.omit
     ))
     begin_processing(
         args.path,
-        args.target,
+        int(args.target),
+        args.lines,
+        args.omit.split(','),
         output_file=args.output_file,
-        test_run=args.test,
         n_lines=int(args.n_lines),
     )
