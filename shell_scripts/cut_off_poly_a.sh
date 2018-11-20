49f8598b2b94902adcb53088fd0d35e7c5f02638
Alexander Shein <mr.ashein@gmail.com>
2018-11-20 22:32:50 +0300

    Moved shell and python scripts to src folder

--- shell_scripts/cut_off_poly_a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# First arg: path
-# Second arg: path
-for filename in $(ls $1); do
-    POLY_A_END=$(tail -n1 $1$filename | rev | grep -aob '[CTG]\{2,\}' | head -n1 | grep -oE '[0-9]+')
-    # Cut the sequence to the position of last non-A bases pair
-    SEQ=$(tail -n1 $1$filename)
-    echo $SEQ | cut -c1-$((${#SEQ}-$POLY_A_END)) >> "$2$filename"
-done;
