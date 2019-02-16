a039af5dd3529e37c345940fc75f9817cc484b1f
Alexander Shein <mr.ashein@gmail.com>
2019-02-16 10:30:27 +0300

    Path fixes for script

--- src/shell_scripts/cut_last_n_bases.sh
+++ src/shell_scripts/cut_last_n_bases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# First arg: path
-# Second arg: filename
+# First arg: target path
+# Second arg: path to save files at
 # Third arg: int
 # Fourth arg: bool (clean)
 # Example: shell_scripts/cut_last_n_bases.sh $(pwd) ./test.txt 50 clean
@@ -13,9 +13,9 @@ for filename in $(ls $1/*.fa); do
         POLY_A_END=$(tail -n1 $filename | rev | grep -aob '[CTG]\{2,\}' | head -n1 | grep -oE '[0-9]+')
         # Cut the sequence to the position of first non-A base from end
         SEQ=$(tail -c $(($3+1+$POLY_A_END)) $filename)
-        echo $SEQ | cut -c1-$((${#SEQ}-$POLY_A_END)) >> "$2"
+        echo $SEQ | cut -c1-$((${#SEQ}-$POLY_A_END)) >> "$2$filename"
     else
-        tail -c $(($3+1)) $filename >> "$2"
+        tail -c $(($3+1)) $filename >> "$2$filename"
     fi
     if (($(($COUNTER%200)) == 0))
     then
