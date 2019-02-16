e509cb083dba4a6a23ea34f4c575668abfab5b17
Alexander Shein <mr.ashein@gmail.com>
2019-02-16 09:52:32 +0300

    Shell script to run annotation

--- src/shell_scripts/run_search_pal.sh
+++ src/shell_scripts/run_search_pal.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# First arg: target path
+
 COUNTER=0
-LEN=$(ls | wc -l)
-for filename in $(ls | shuf | head -n$LEN); do
-    /home/xelmar/Work/6_year/transp/src/cpp/search $filename 123.ptt 10 20 0 8 5
+LEN=$(ls $1 | wc -l)
+for filename in $(ls $1 | shuf | head -n$LEN); do
+    /home/xelmar/Work/6_year/transp/src/cpp/search $1$filename 123.ptt 10 20 0 8 5
     COUNTER=$[COUNTER + 1]
     if (($(($COUNTER%100)) == 0))
     then
